0fdf4d3138c45b2f82f9d85f5756cb25c4ae367c
Manny <tristan.michel@epitech.eu>
2023-08-07 17:06:04 +0200

    size of table

--- main.py
+++ main.py
@@ -3,108 +3,104 @@ import requests
 from bs4 import BeautifulSoup
 from terminaltables import SingleTable
 import sys
+import time
 from colorama import Fore, Style
 from token_config import URL_TOKEN
-
-# Import de la fonction display_banner depuis le fichier banner.py
+import os
 from static.banner import display_banner
 
-# Affichage de la bannière avec des couleurs
-print(Fore.RED + display_banner() + Style.RESET_ALL)
+# Display banner on startup
+banner_lines = display_banner()
+for line in banner_lines:
+    print(Fore.RED + line + Style.RESET_ALL)
+    time.sleep(0.1)
+
+def get_terminal_width():
+    """Get the current width of the terminal."""
+    rows, columns = os.popen('stty size', 'r').read().split()
+    return int(columns)
+
+def adjust_table_width(table_instance):
+    """Adjust the width of the table according to terminal's width."""
+    terminal_width = get_terminal_width()
+    num_columns = len(table_instance.table_data[0])
+    column_width = terminal_width // num_columns
+    table_instance.column_max_width = {index: column_width - 3 for index in range(num_columns)}
 
 def pass_the_captcha():
-
+    """Handles the CAPTCHA challenge for the given onion website."""
     url_index = "http://4wbwa6vcpvcr3vvf4qkhppgy56urmjcj2vagu2iqgp3z656xcmfdbiqd.onion.pet/"
-
     req = requests.get(url_index, verify=False)
     soup = BeautifulSoup(req.text, "html.parser")
     get_captcha = soup.find('pre').text
     get_id = soup.find('input', {'name': 'id'}).get('value')
     print(get_captcha)
 
-    captcha = input("Entrez le captcha: ")
+    captcha = input("Enter the captcha: ")
     url_captcha = "{}captcha".format(url_index)
 
     datas = {
         "captcha": captcha,
         "id": get_id
     }
-    
+
     req_captcha = requests.post(url_captcha, verify=False, data=datas)
     with open("token_config.py", "w") as replace_token:
         replace_token.write("""URL_TOKEN = "{}" """.format(req_captcha.url.split("=")[-1]))
     main(req_captcha.url.split("=")[-1])
 
 def main(URL_TOKEN):
-    # Filtrage des paramètres vides
+    """Main function to scrape and display data from the onion website."""
+    # Filter out parameters that are empty
     filtered_params = {key: value for key, value in params.items() if value}
-
-    # Construction de l'URL de recherche avec les paramètres filtrés
     search_url = "http://4wbwa6vcpvcr3vvf4qkhppgy56urmjcj2vagu2iqgp3z656xcmfdbiqd.onion.pet/search?" + "&".join(f"{key}={value}" for key, value in filtered_params.items())
-
-    # Ajout des paramètres fixes à l'URL de recherche
     search_url += "&s={}&r=*any*&g=*any*".format(URL_TOKEN)
-
-    # Effectuer la requête de recherche
     response = requests.get(search_url, verify=False, allow_redirects=True)
-
-    # Traitement de la réponse avec BeautifulSoup
     soup = BeautifulSoup(response.content, 'html.parser')
 
+    # Handle CAPTCHA if presented
     if "fill" in response.text:
         pass_the_captcha()
     else:
-
         table = soup.find('table')
-
         if table:
-            # Extraction des données de la table
             headers = [th.text.strip() for th in table.find_all('th')]
             data = []
             for row in table.find_all('tr')[1:]:
                 row_data = [td.text.strip() for td in row.find_all('td')]
                 data.append(row_data)
 
-            # Création d'un objet SingleTable avec les en-têtes et les données
+            # Adjust and print the data table
             table_instance = SingleTable([headers] + data)
-
-            # Rendre la table responsive en fonction de la taille du terminal
+            adjust_table_width(table_instance)
             table_instance.inner_heading_row_border = False
             table_instance.inner_row_border = True
             table_instance.justify_columns = {index: 'center' for index in range(len(headers))}
-
-            # Affichage de la table
             print(table_instance.table)
-
-            # Ajout d'un espace et d'un titre
             print("\nDirect Link to Facebook profile:\n")
-
-            # Affichage des URL de profils Facebook en utilisant les ID de la table
             for row in data:
                 fb_url = f"https://www.facebook.com/profile.php?id={row[0]}"
                 print(fb_url)
-
         else:
-            print("Aucun résultat.")
+            print("No results found.")
 
 if __name__ == '__main__':
-
-    # Analyseur pour les arguments de la ligne de commande
-    parser = argparse.ArgumentParser(description='Outil pour rechercher des informations dans un dump Facebook')
+    # Parse arguments from command line
+    parser = argparse.ArgumentParser(description='Tool to search for information in a Facebook dump')
     parser.add_argument('-i', '--id', help='ID')
-    parser.add_argument('-f', '--firstname', help='prénom')
-    parser.add_argument('-l', '--lastname', help='nom de famille')
-    parser.add_argument('-t', '--phone', help='téléphone')
-    parser.add_argument('-w', '--work', help='travail')
-    parser.add_argument('-o', '--location', help='localisation')
+    parser.add_argument('-f', '--firstname', help='First name')
+    parser.add_argument('-l', '--lastname', help='Last name')
+    parser.add_argument('-t', '--phone', help='Phone number')
+    parser.add_argument('-w', '--work', help='Work')
+    parser.add_argument('-o', '--location', help='Location')
     args = parser.parse_args()
 
-    # Vérification si aucun argument n'a été fourni
+    # Check if any arguments were provided
     if not any(vars(args).values()):
         parser.print_help()
         sys.exit(1)
 
-    # Construction des paramètres de recherche pour l'URL
+    # Create a dictionary with all the provided search parameters
     params = {
         'i': args.id if args.id else '',
         'f': args.firstname if args.firstname else '',
